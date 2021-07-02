6252797872fdc6ccc8744ab3509580dd251bb4af
adg <adamgatnar@gmail.com>
2021-07-02 17:35:13 +0200

    new snapshot

--- create_disk.sh
+++ create_disk.sh
@@ -1,1 +1,1 @@
-gcloud compute disks create dcs-balanced --project=project-dcs --type=pd-balanced --size=200GB --zone=europe-west4-a --source-snapshot=dcs-update1-snapshot
+gcloud compute disks create dcs-balanced --project=project-dcs --type=pd-balanced --size=200GB --zone=europe-west4-a --source-snapshot=dcs-update2-snapshot
